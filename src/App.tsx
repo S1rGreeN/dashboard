32874c272620675652becd1b9bec226765160928
Alejandro Verdesoto <s1rgreen.dev@gmail.com>
2025-11-26 19:18:13 +0000

    funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@
 //import viteLogo from '/vite.svg'
 import { Grid } from '@mui/material';
 import './App.css'
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
+
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -12,10 +15,10 @@ function App() {
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado <HeaderUI/></Grid>
 
       {/* Alertas */}
-      <Grid >Elemento: Alertas</Grid>
+      <Grid container justifyContent="right" alignItems="center">Elemento: Alertas<AlertUI description="No se preveen lluvias"/></Grid>
 
       {/* Selector */}
       <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
---
+++ src/components/AlertUI.tsx
@@ -0,0 +1,10 @@
+import Alert from '@mui/material/Alert';
+interface AlertConfig {
+    description: string;
+}
+
+export default function AlertUI( config:AlertConfig ) {
+    return (
+        <Alert variant="standard" severity="success"> {config.description} </Alert>
+    )
+}
