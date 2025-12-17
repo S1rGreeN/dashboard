99b19bc3ff81e6f11a05e821a75861aeab6e2b6d
Alejandro Verdesoto <s1rgreen.dev@gmail.com>
2025-12-17 03:19:38 +0000

    correccion

--- src/components/ExtraUI.tsx
+++ src/components/ExtraUI.tsx
@@ -1,7 +1,7 @@
  import Card from '@mui/material/Card';
  import CardContent from '@mui/material/CardContent';
  import Typography from '@mui/material/Typography';
- import type {proceDatita} from '../functions/useFetchData';
+ 
 
  interface ExtraUIProps {
      title?: string;
